350879168b6015b58fe686945144becb3e7d7229
Mairah <animefreaknor@hotmail.com>
2019-08-21 21:40:04 +1000

    Added Chat component

--- server/socket.js
+++ server/socket.js
@@ -4,9 +4,12 @@ module.exports = {
             // Output to server console when request comes in
             console.log('User connection on port ' + PORT + ' : ' + socket.id);
 
+            // Subscribe socket to given channel
+            socket.join('room1');
+
             // When message comes in, emit it back to all sockets with the message.
             socket.on('message', (message) => {
-                io.emit('message', message);
+                io.in('room1').emit('message', message);
             });
         });
     }
