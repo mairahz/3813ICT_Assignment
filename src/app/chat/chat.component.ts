4a62697b5ecade637a2f101181b44672976c87d1
Mairah <animefreaknor@hotmail.com>
2019-08-21 21:46:20 +1000

    Changed socket.service and chat ts

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketService } from '../services/socket.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  messagecontent: string = "";
+  messages: string[] = [];
+  ioConnection:any;
+
+  constructor(private socketService:SocketService) { }
 
   ngOnInit() {
+    this.initIoConnection();
   }
 
+  private initIoConnection(){
+    this.socketService.initSocket();
+    this.ioConnection = this.socketService.onMessage()
+      .subscribe((message: string) => {
+        // Add new message to the messages array
+        this.messages.push(message);
+      });
+  }
+
+  private chat(){
+    if(this.messagecontent){
+      // Check there is a message to send
+      this.socketService.send(this.messagecontent);
+      this.messagecontent = null;
+    } else {
+      console.log("No message");
+    }
+  }
+  
 }
