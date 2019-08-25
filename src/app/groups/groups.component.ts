c8d46d16cf4b3f33e64e27a36e52d9edc1e6b47f
Mairah <animefreaknor@hotmail.com>
2019-08-25 21:16:57 +1000

    Created new group form

--- src/app/groups/groups.component.ts
+++ src/app/groups/groups.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-groups',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groups.component.css']
 })
 export class GroupsComponent implements OnInit {
+  groups: string[] =[];
+  newGroup: boolean = false;
+  name: string = "";
+  
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClickGroup(){
+    if(!this.newGroup){
+      this.newGroup = true;
+    } else {
+      this.newGroup = false;
+    }
+  }
+
+  itemSubmit(){
+    console.log(this.name);
   }
 
 }
